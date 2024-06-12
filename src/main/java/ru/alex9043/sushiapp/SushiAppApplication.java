7c106d960fe029537f13310ee3431d7da88f406b
alex9043 <alex980127@gmail.com>
2024-06-12 09:47:30 +0300

    Init App

---
+++ src/main/java/ru/alex9043/sushiapp/SushiAppApplication.java
@@ -0,0 +1,13 @@
+package ru.alex9043.sushiapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SushiAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SushiAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/alex9043/sushiapp/SushiAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.alex9043.sushiapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SushiAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
